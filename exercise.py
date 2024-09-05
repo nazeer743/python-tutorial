96764c558416f4ca0ed1c09f88b4cb58f7035e98
nazeer743 <itsnazeerxd@gmail.com>
2024-09-05 22:09:08 +0530

    modified

--- exercise.py
+++ exercise.py
@@ -1,8 +1,8 @@
-weight = input(weight: )
-unit = input("(K)kgs or (L)lbs")
+weight = int(input("weight: "))
+unit = input("(K)kgs or (L)lbs: ")
 if unit.upper() == "K":
     converted = weight / 0.45
-    print("weight in lbs: " + converted)
+    print("weight in lbs: " +str(converted))
 else:
     converted= weight* 0.45
-    print("weight in kg: " + converted)
+    print("weight in kg: " +str( converted))
