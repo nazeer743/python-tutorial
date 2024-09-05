93e6d1a608cf616d5cb7f34593f05b877dc31165
nazeer743 <itsnazeerxd@gmail.com>
2024-09-05 19:45:04 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+name = input("What is yout name? ")
+print("Hello " + name)
+
+birth_year=input("Enter your birth year:")
+age = 2024 - int(birth_year)
+print(age)
+
+x = input("first: ")
+y = input("second: ")
+sum = float(x) + float(y)
+print(sum)  
